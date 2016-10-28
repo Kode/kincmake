fb070a022f3b3ae6680da44bdf1d40162ac2dc7c
Robert Konrad <git2016@robdangero.us>
2016-10-29 01:38:12 +0200

    Add CLion export for Linux

--- out/Exporters/LinuxExporter.js
+++ out/Exporters/LinuxExporter.js
@@ -10,6 +10,7 @@ class LinuxExporter extends Exporter_1.Exporter {
     exportSolution(project, from, to, platform, vrApi, nokrafix, options) {
         this.exportMakefile(project, from, to, platform, vrApi, nokrafix, options);
         this.exportCodeBlocks(project, from, to, platform, vrApi, nokrafix, options);
+        this.exportCLion(project, from, to, platform, vrApi, nokrafix, options);
     }
     exportMakefile(project, from, to, platform, vrApi, nokrafix, options) {
         let objects = {};
@@ -219,6 +220,47 @@ class LinuxExporter extends Exporter_1.Exporter {
         this.p('</Extensions>', 2);
         this.p('</Project>', 1);
         this.p('</CodeBlocks_project_file>');
+        this.closeFile();
+    }
+    exportCLion(project, from, to, platform, vrApi, nokrafix, options) {
+        const indir = path.join(__dirname, '..', '..', 'Data', 'linux');
+        fs.ensureDirSync(path.resolve(to, project.getName(), '.idea'));
+        let misc = fs.readFileSync(path.join(indir, 'idea', 'misc.xml'), 'utf8');
+        misc = misc.replace(/{root}/g, path.resolve(from));
+        fs.writeFileSync(path.join(to, project.getName(), '.idea', 'misc.xml'), misc, 'utf8');
+        this.writeFile(path.resolve(to, project.getName(), 'CMakeLists.txt'));
+        let name = project.getName().replace(/ /g, '_');
+        this.p('cmake_minimum_required(VERSION 3.6)');
+        this.p('project(' + name + ')');
+        if (project.cpp11) {
+            this.p('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -pthread -static-libgcc -static-libstdc++")');
+        }
+        else {
+            this.p('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -static-libgcc -static-libstdc++")');
+        }
+        let defines = '';
+        for (let def of project.getDefines()) {
+            defines += '  -D' + def + '\n';
+        }
+        this.p('add_definitions(\n' + defines + ')');
+        let includes = '';
+        for (let inc of project.getIncludeDirs()) {
+            includes += '  "' + path.resolve(inc).replace(/\\/g, '/') + '"\n';
+        }
+        this.p('include_directories(\n' + includes + ')');
+        let files = '';
+        for (let file of project.getFiles()) {
+            if (file.file.endsWith('.c') || file.file.endsWith('.cc') || file.file.endsWith('.cpp') || file.file.endsWith('.h')) {
+                files += '  "' + path.resolve(file.file).replace(/\\/g, '/') + '"\n';
+            }
+        }
+        this.p('set(SOURCE_FILES\n' + files + ')');
+        this.p('add_executable(' + name + ' ${SOURCE_FILES})');
+        let libraries = '';
+        for (let lib of project.getLibs()) {
+            libraries += '  ' + lib + '\n';
+        }
+        this.p('target_link_libraries(ShaderTest\n' + libraries + ')');
         this.closeFile();
     }
 }
--- src/Exporters/LinuxExporter.ts
+++ src/Exporters/LinuxExporter.ts
@@ -11,10 +11,11 @@ export class LinuxExporter extends Exporter {
 		super();
 	}
 
-    exportSolution(project: Project, from: string, to: string, platform: string, vrApi: any, nokrafix: boolean, options: any) {
-        this.exportMakefile(project, from, to, platform, vrApi, nokrafix, options);
-        this.exportCodeBlocks(project, from, to, platform, vrApi, nokrafix, options);
-    }
+	exportSolution(project: Project, from: string, to: string, platform: string, vrApi: any, nokrafix: boolean, options: any) {
+		this.exportMakefile(project, from, to, platform, vrApi, nokrafix, options);
+		this.exportCodeBlocks(project, from, to, platform, vrApi, nokrafix, options);
+		this.exportCLion(project, from, to, platform, vrApi, nokrafix, options);
+	}
 	
 	exportMakefile(project: Project, from: string, to: string, platform: string, vrApi: any, nokrafix: boolean, options: any) {
 		let objects: any = {};
@@ -239,4 +240,56 @@ export class LinuxExporter extends Exporter {
 		this.p('</CodeBlocks_project_file>');
 		this.closeFile();
 	}
+
+	exportCLion(project: Project, from: string, to: string, platform: string, vrApi: any, nokrafix: boolean, options: any) {
+		const indir = path.join(__dirname, '..', '..', 'Data', 'linux');
+		fs.ensureDirSync(path.resolve(to, project.getName(), '.idea'));
+		let misc = fs.readFileSync(path.join(indir, 'idea', 'misc.xml'), 'utf8');
+		misc = misc.replace(/{root}/g, path.resolve(from));
+		fs.writeFileSync(path.join(to, project.getName(), '.idea', 'misc.xml'), misc, 'utf8');
+
+		this.writeFile(path.resolve(to, project.getName(), 'CMakeLists.txt'));
+
+		let name = project.getName().replace(/ /g, '_');
+
+		this.p('cmake_minimum_required(VERSION 3.6)');
+		this.p('project(' + name + ')');
+		
+		if (project.cpp11) {
+			this.p('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -pthread -static-libgcc -static-libstdc++")');
+		}
+		else {
+			this.p('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -static-libgcc -static-libstdc++")');
+		}
+
+		let defines = '';
+		for (let def of project.getDefines()) {
+			defines += '  -D' + def + '\n';
+		}
+		this.p('add_definitions(\n' + defines + ')');
+
+		let includes = '';
+		for (let inc of project.getIncludeDirs()) {
+			includes += '  "' + path.resolve(inc).replace(/\\/g, '/') + '"\n';
+		}
+		this.p('include_directories(\n' + includes + ')');
+
+		let files = '';
+		for (let file of project.getFiles()) {
+			if (file.file.endsWith('.c') || file.file.endsWith('.cc') || file.file.endsWith('.cpp') || file.file.endsWith('.h')) {
+				files += '  "' + path.resolve(file.file).replace(/\\/g, '/') + '"\n';
+			}
+		}
+		this.p('set(SOURCE_FILES\n' + files + ')')
+
+		this.p('add_executable(' + name + ' ${SOURCE_FILES})');
+
+		let libraries = '';
+		for (let lib of project.getLibs()) {
+			libraries += '  ' + lib + '\n';
+		}
+		this.p('target_link_libraries(ShaderTest\n' + libraries + ')');
+
+		this.closeFile();
+    }
 }
