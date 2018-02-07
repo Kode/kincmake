0cf49d893df0db66c45ea76ac5d92fe0d3db4d4d
Robert Konrad <git2016@robdangero.us>
2018-02-07 21:27:03 +0100

    Require a node.js 8.9 or higher

--- koremake.js
+++ koremake.js
@@ -1,1 +1,6 @@
+if (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 8.9 && process.version !== 'v7.4.0' /* Kode Studio 17.9 */) {
+	console.error('Requires Node.js version 8.9 or higher but found ' + process.version + '.');
+	process.exit(1);
+}
+
 require('./out/koremake.js');
