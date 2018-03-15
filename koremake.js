346d01c479aeacb43cbdb3f4b173b3bae179b439
Robert Konrad <git2016@robdangero.us>
2018-03-15 13:40:12 +0100

    Fix Node.js version check

--- koremake.js
+++ koremake.js
@@ -1,4 +1,5 @@
-if (Number(process.version.match(/^v(\d+\.\d+)/)[1]) < 8.9 && process.version !== 'v7.4.0' /* Kode Studio 17.9 */) {
+let versions = process.version.substring(1).split('.');
+if ((Number(versions[0]) < 8 || (Number(versions[0]) === 8 && Number(versions[1]) < 9)) && process.version !== 'v7.4.0' /* Kode Studio 17.9 */) {
 	console.error('Requires Node.js version 8.9 or higher but found ' + process.version + '.');
 	process.exit(1);
 }
