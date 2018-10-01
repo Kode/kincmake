a01a8eeae381c38ce54b20490680b8ae175d1535
RblSb <msrblsb@gmail.com>
2018-10-01 17:58:51 +0300

    Fix comparison

--- out/Exporters/AndroidExporter.js
+++ out/Exporters/AndroidExporter.js
@@ -85,22 +85,14 @@ class AndroidExporter extends Exporter_1.Exporter {
         }
         cmake = cmake.replace(/{libraries1}/g, libraries1).replace(/{libraries2}/g, libraries2);
         // prevent overwriting CMakeLists.txt if it has not changed
-        var oldPath = path.join(outdir, 'app', 'CMakeLists.txt');
-        var newPath = path.join(outdir, 'app', 'CMakeLists_new.txt');
-        if (!fs.existsSync(oldPath)) {
-            fs.writeFileSync(oldPath, cmake, { encoding: 'utf8' });
+        const cmakePath = path.join(outdir, 'app', 'CMakeLists.txt');
+        if (!fs.existsSync(cmakePath)) {
+            fs.writeFileSync(cmakePath, cmake, { encoding: 'utf8' });
         }
         else {
-            fs.writeFileSync(newPath, cmake, { encoding: 'utf8' });
-            var file1 = fs.readFileSync(oldPath, 'utf8');
-            var file2 = fs.readFileSync(newPath, 'utf8');
-            if (file1 === file2) {
-                fs.unlinkSync(newPath);
-            }
-            else {
-                fs.unlinkSync(oldPath);
-                fs.renameSync(newPath, oldPath);
-            }
+            const file = fs.readFileSync(cmakePath, 'utf8');
+            if (file !== cmake)
+                fs.writeFileSync(cmakePath, cmake, { encoding: 'utf8' });
         }
         fs.copySync(path.join(indir, 'app', 'proguard-rules.pro'), path.join(outdir, 'app', 'proguard-rules.pro'));
         fs.ensureDirSync(path.join(outdir, 'app', 'src'));
