78d5f66e0bbb3956a56a63fdc03073fa3f20348d
Robert Konrad <git2016@robdangero.us>
2020-10-19 18:21:05 +0200

    Add Linux vscode support

--- out/Exporters/VSCodeExporter.js
+++ out/Exporters/VSCodeExporter.js
@@ -8,6 +8,39 @@ class VSCodeExporter extends Exporter_1.Exporter {
     constructor() {
         super();
     }
+    configName(platform) {
+        if (platform === 'windows') {
+            return 'Win32';
+        }
+        else if (platform === 'linux') {
+            return 'Linux';
+        }
+        else {
+            return 'unknown platform';
+        }
+    }
+    compilerPath(platform) {
+        if (platform === 'windows') {
+            return 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.27.29110/bin/Hostx64/x64/cl.exe';
+        }
+        else if (platform === 'linux') {
+            return '/usr/bin/gcc';
+        }
+        else {
+            return 'unknown platform';
+        }
+    }
+    intelliSenseMode(platform) {
+        if (platform === 'windows') {
+            return 'msvc-x64';
+        }
+        else if (platform === 'linux') {
+            return 'gcc-x64';
+        }
+        else {
+            return 'unknown platform';
+        }
+    }
     async exportSolution(project, from, to, platform, vrApi, options) {
         fs.ensureDirSync(path.join(from, '.vscode'));
         this.writeFile(path.join(from, '.vscode', 'c_cpp_properties.json'));
@@ -27,14 +60,14 @@ class VSCodeExporter extends Exporter_1.Exporter {
         const data = {
             configurations: [
                 {
-                    name: 'Win32',
+                    name: this.configName(platform),
                     includePath: includes,
                     defines: defines,
                     windowsSdkVersion: '10.0.19041.0',
-                    compilerPath: 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.27.29110/bin/Hostx64/x64/cl.exe',
+                    compilerPath: this.compilerPath(platform),
                     cStandard: 'c11',
                     cppStandard: 'c++17',
-                    intelliSenseMode: 'msvc-x64'
+                    intelliSenseMode: this.intelliSenseMode(platform)
                 }
             ]
         };
--- src/Exporters/VSCodeExporter.ts
+++ src/Exporters/VSCodeExporter.ts
@@ -8,6 +8,42 @@ export class VSCodeExporter extends Exporter {
 		super();
 	}
 
+	configName(platform: string): string {
+		if (platform === 'windows') {
+			return 'Win32';
+		}
+		else if (platform === 'linux') {
+			return 'Linux';
+		}
+		else {
+			return 'unknown platform';
+		}
+	}
+
+	compilerPath(platform: string): string {
+		if (platform === 'windows') {
+			return 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.27.29110/bin/Hostx64/x64/cl.exe';
+		}
+		else if (platform === 'linux') {
+			return '/usr/bin/gcc';
+		}
+		else {
+			return 'unknown platform';
+		}
+	}
+
+	intelliSenseMode(platform: string): string {
+		if (platform === 'windows') {
+			return 'msvc-x64';
+		}
+		else if (platform === 'linux') {
+			return 'gcc-x64';
+		}
+		else {
+			return 'unknown platform';
+		}
+	}
+
 	async exportSolution(project: Project, from: string, to: string, platform: string, vrApi: any, options: any) {
 		fs.ensureDirSync(path.join(from, '.vscode'));
 		this.writeFile(path.join(from, '.vscode', 'c_cpp_properties.json'));
@@ -30,14 +66,14 @@ export class VSCodeExporter extends Exporter {
 		const data = {
 			configurations: [
 				{
-					name: 'Win32',
+					name: this.configName(platform),
 					includePath: includes,
 					defines: defines,
 					windowsSdkVersion: '10.0.19041.0',
-					compilerPath: 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.27.29110/bin/Hostx64/x64/cl.exe',
+					compilerPath: this.compilerPath(platform),
 					cStandard: 'c11',
 					cppStandard: 'c++17',
-					intelliSenseMode: 'msvc-x64'
+					intelliSenseMode: this.intelliSenseMode(platform)
 				}
 			]
 		};
